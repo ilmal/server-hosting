289104d2f22fe681269623ad22ed9fd59d2b6a99
ilmal <nils@u1.se>
2023-03-22 00:13:44 +0100

    adding stuff

--- src/backend/src/main.py
+++ src/backend/src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
 from modules.create_user import create_user
+from modules.create_server import create_server
 
 app = Flask(__name__)
 
@@ -8,10 +9,14 @@ def response(input):
     if input: return input, 200
     return "", 200
 
-@app.route("/")
+@app.route("/api/")
 def hello_world():
     return "Hello world"
 
 @app.route("/api/create_user", methods=["POST"])
 def run_create_user(): 
     return response(create_user(request))
+
+@app.route("/api/create_server", methods=["POST"])
+def run_create_server():
+    return response(create_server(request))
